496f5d719280140ba9d0394c1ecb9293163a9f6b
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-10 15:50:29 +0300

    glLearning: main add while loop

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <GL/glew.h>
+#include "Display.h"
 
 int main(int argn, char *argc[]){
 	for(int i = 0; i < argn; i++)
 		std::cout << argc[i] << std::endl;
 	std::cout << "Hello world" <<std::endl;
+	
+	Display display(800, 600, "Hello OpenGL");
+	while (!dislplay.IsClosed()){
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		display.Update();
+	}
 	return 0;
 }
