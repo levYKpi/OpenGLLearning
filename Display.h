32355d12a78d0ffed21169b5c410172591327316
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-10 15:45:24 +0300

    glLearning: display class add initialization and exition

--- Display.h
+++ Display.h
@@ -2,14 +2,19 @@
 #define __DISPLAY_H__
 
 #include <string>
+#include <SDL2/SDL.h>
 
 class Display{
 public:
 	Display(int width, int height, const std::string& title);
 	virtual ~Display();
+	void Update();
+	bool isClosed();
 private:
+	SDL_Window* m_window; 
+	SDL_GLContext m_glContext;
+	bool m_isClosed;
 protected:
-	
 };
 
 #endif
