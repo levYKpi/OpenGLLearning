0836b5040c6998eab8db8d85500f6e8f2ff2e40e
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-11 15:54:06 +0300

    glLearinig: add shaders in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "Display.h"
+#include "Shader.h"
 
 int main(int argn, char *argc[]){
 	for(int i = 0; i < argn; i++)
@@ -8,8 +9,10 @@ int main(int argn, char *argc[]){
 	std::cout << "Hello world" <<std::endl;
 	
 	Display display(800, 600, "Hello OpenGL");
+	Shader shader("./res/BaseShader");
 	while (!display.IsClosed()){
 		display.Clear();
+		shader.Bind();
 		display.Update();
 	}
 	return 0;
